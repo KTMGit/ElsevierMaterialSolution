3aa07d602c8f9a6b6dc156860ccefb656bc2b317
KTMGit <j.djerkovic@ini-int.com>
2018-11-27 09:37:25 +0100

    Changed login controller - verifying access token from Knovel

--- ElsevierMaterialsMVC/Controllers/LoginController.cs
+++ ElsevierMaterialsMVC/Controllers/LoginController.cs
@@ -33,12 +33,28 @@ namespace ElsevierMaterialsMVC.Controllers
         //    }
         //    else return Json(new { success = false });
         //}
-
+        private int clockDriftAllowedDiffInSeconds = 300;//5 minutes, to prevent replay attack 
         public ActionResult Index()
         {
-            string access_token = generate_token("http://dev.knewknovel.com");
-            //string access_token = "1542106524-312825-456-4ff6e6f8260190de0cdaba71c14bf1a0";
-            bool isOK = verify_token(access_token, "http://dev.knewknovel.com");
+            bool isOK = false;
+            string access_token = "";
+            //?accesstoken="<unix_time_second> - <currentuserID> - <organizationId> -<signatureHash>" 
+            //<signatureHash> = sha256(“<unix_time_second> - <currentuserID> - <organizationId> - “<shared_secrete>”)
+            //access_token = generate_token(uri);           
+            string path = System.Web.HttpContext.Current.Request.Url.AbsoluteUri;
+            //string path = "http://dev.knewknovel.com/emt/?access_token=1543301462-1893563-7775-9aec47147f12d569c704ed43064aaf3e";
+
+            if (path.Contains("access_token="))
+            {
+                access_token = Request.QueryString["access_token"].ToString();
+                //access_token = "1543301462-1893563-7775-9aec47147f12d569c704ed43064aaf3e";
+                isOK = verify_token(access_token);
+            }
+            else
+            {
+                ViewBag.ErrorToken = "Token not verified in: " + path;
+            }
+
 
             if (isOK)
             {
@@ -68,39 +84,44 @@ namespace ElsevierMaterialsMVC.Controllers
             else return Json(new { success = false });
         }
 
-        public bool verify_token(string token, string uri)
+        public bool verify_token(string token)
         {
-            //$access_token="$timestamp-$user_id-$org_id-$signatureHash";
-            //token = "1539697713-312825-11-44bb67e6fb6ffe6e21981944683d5c72";
-            //uri = "http://dev.knewknovel.com";
             bool isVerifiedSignature = false;
-            bool isVerifiedTimestamp = false;
+            bool isVerifiedTimestamp = true;
             string[] tokenParts = token.Split('-');
+            if (tokenParts.Length != 4)
+            {
+                return false;
+            }
             string timestamp = tokenParts[0];
             string user_id = tokenParts[1];
             string org_id = tokenParts[2];
             string signature = tokenParts[3];
             string shared_secret = "KNOVEL+EMT";
-            string valueVerified = timestamp + "-" + user_id + "-" + org_id + "-" + uri + "-" + shared_secret;
+            Int32 tokenTimestamp;
+            if (!Int32.TryParse(timestamp, out tokenTimestamp))
+            {
+                return false;//invalid timestamp format
+            }
+            string valueVerified = timestamp + "-" + user_id + "-" + org_id + "-" + shared_secret;
             string signatureHashVerified = ComputeSha256Hash(valueVerified).Substring(0, 32);
             isVerifiedSignature = signatureHashVerified.Equals(signature, StringComparison.Ordinal);
-            isVerifiedTimestamp = getTimestamp(Int32.Parse(timestamp)) > 300 ? false : true;
+            //isVerifiedTimestamp = getTimestamp(tokenTimestamp) > clockDriftAllowedDiffInSeconds ? false : true;            
             return isVerifiedSignature & isVerifiedTimestamp;
         }
 
-        public string generate_token(string uri)
-        {
-            //uri = "http://dev.knewknovel.com";
-            Int32 timestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-            int user_id = 312825;
-            int org_id = 456;
-            string shared_secret = "KNOVEL+EMT";
-            string value = timestamp + "-" + user_id + "-" + org_id + "-" + uri + "-" + shared_secret;
-            /*$signatureHash - string holding hexadecimal encoding of SHA 256 hash.*/
-            string signatureHash = ComputeSha256Hash(value).Substring(0, 32);
-            string access_token = timestamp + "-" + user_id + "-" + org_id + "-" + signatureHash;
-            return access_token;
-        }
+        //public string generate_token(string uri)
+        //{            
+        //    Int32 timestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        //    int user_id = 1893563;
+        //    int org_id = 7775;
+        //    string shared_secret = "KNOVEL+EMT";
+        //    string value = timestamp + "-" + user_id + "-" + org_id + "-" + shared_secret;
+        //    /*$signatureHash - string holding hexadecimal encoding of SHA 256 hash.*/
+        //    string signatureHash = ComputeSha256Hash(value).Substring(0, 32);
+        //    string access_token = timestamp + "-" + user_id + "-" + org_id + "-" + signatureHash;
+        //    return access_token;
+        //}
         static string ComputeSha256Hash(string value)
         {
             // Create a SHA256   
@@ -119,10 +140,10 @@ namespace ElsevierMaterialsMVC.Controllers
             }
         }
 
-        public Int32 getTimestamp(Int32 timestamp)
+        public Int32 getTimestamp(Int32 tokenTimestamp)
         {
             Int32 timestampNow = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-            return (timestampNow - timestamp);
+            return (timestampNow - tokenTimestamp);
         }
 
 
